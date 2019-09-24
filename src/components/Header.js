7b02c85ccdb178bfab9b2219f38e881884b7d148
Patrick Shushereba <pshushereba@gmail.com>
2019-09-24 12:58:19 -0700

    Added reducer.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
+import {connect} from 'react-redux';
+import {featuresReducer} from '../reducers/featuresReducer.js';
+
 const Header = props => {
+  console.log("In Header Props", props)
   return (
     <>
       <figure className="image is-128x128">
@@ -12,4 +16,14 @@ const Header = props => {
   );
 };
 
-export default Header;
+const mapStateToProps = (state) => {
+  console.log("In mSTP", state.car.name);
+  return {
+    name: state.car.name,
+    image: state.car.image
+  };
+  
+}
+
+export default connect(mapStateToProps,
+  {})(Header)
--- src/index.js
+++ src/index.js
@@ -2,8 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// Import Provider
+import {Provider} from 'react-redux';
+
+// Import createStore
+import {createStore} from 'redux';
+
+// Import Reducers
+import {featuresReducer} from './reducers/featuresReducer'
+
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = createStore(featuresReducer);
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, rootElement);
