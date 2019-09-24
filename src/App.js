bb90e72d063e3af2e5299a6dd251b0510af7e431
Patrick Shushereba <pshushereba@gmail.com>
2019-09-24 14:33:19 -0700

    Started connecting components to redux store.

--- src/App.js
+++ src/App.js
@@ -35,11 +35,11 @@ const App = () => {
     <div className="boxes">
       <div className="box">
         <Header />
-        {/* <AddedFeatures /> */}
+        <AddedFeatures />
       </div>
       <div className="box">
-        {/* <AdditionalFeatures />
-        <Total /> */}
+        <AdditionalFeatures />
+        {/* <Total /> */}
       </div>
     </div>
   );
--- src/components/AdditionalFeatures.js
+++ src/components/AdditionalFeatures.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import AdditionalFeature from './AdditionalFeature';
 
+import {connect} from 'react-redux';
+
 const AdditionalFeatures = props => {
   return (
     <div className="content">
@@ -18,4 +20,10 @@ const AdditionalFeatures = props => {
   );
 };
 
-export default AdditionalFeatures;
+const mapStateToProps = (state) => {
+  return {
+    store: state.store
+  }
+}
+
+export default connect(mapStateToProps, {})(AdditionalFeatures);
